436cf6eef10d7aace7270cb8b06c4e3b3815d239
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-04-27 12:43:13 -0300

    aula 3

--- lib/rocketpay_web/views/error_view.ex
+++ lib/rocketpay_web/views/error_view.ex
@@ -1,6 +1,8 @@
 defmodule RocketpayWeb.ErrorView do
   use RocketpayWeb, :view
 
+  import Ecto.Changeset, only: [traverse_errors: 2]
+  alias Ecto.Changeset
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.json", _assigns) do
@@ -16,5 +18,19 @@ defmodule RocketpayWeb.ErrorView do
         detail: Phoenix.Controller.status_message_from_template(template)
       }
     }
+  end
+
+  def render("400.json", %{result: %Changeset{} = changeset}) do
+    %{
+      message: translate_errors(changeset)
+    }
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
