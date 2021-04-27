33eb3ac84cea85bc93ac41e8e4a344cbde1deaf0
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-04-27 10:00:15 -0300

    aula 2

--- config/config.exs
+++ config/config.exs
@@ -23,6 +23,10 @@ config :rocketpay, RocketpayWeb.Endpoint,
   pubsub_server: Rocketpay.PubSub,
   live_view: [signing_salt: "mr9b3OuT"]
 
+config :rocketpay, Rocketpay.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
