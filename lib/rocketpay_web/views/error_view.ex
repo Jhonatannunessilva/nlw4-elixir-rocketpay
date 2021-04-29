10a7d8be36dc245e3c2b407ad664d5f61299b79b
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-04-29 11:25:18 -0300

    aula 4

--- lib/rocketpay_web/views/error_view.ex
+++ lib/rocketpay_web/views/error_view.ex
@@ -26,6 +26,12 @@ defmodule RocketpayWeb.ErrorView do
     }
   end
 
+  def render("400.json", %{result: message}) do
+    %{
+      message: message
+    }
+  end
+
   defp translate_errors(changeset) do
     traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
